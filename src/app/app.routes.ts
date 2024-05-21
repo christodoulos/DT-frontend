5519ca8bbb9fa8f56dda672e7c97d265d55e6a6e
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2024-05-21 16:05:00 +0300

    footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./components/home/home.routes').then((m) => m.routes),
+  },
+];
